29541ad88023c0a46314d363bf3a7181caf4194c
Andrew Trice <amtrice@us.ibm.com>
2022-01-19 01:17:34 -0500

    updated module ref in output
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>

--- test/stages/stage4-outputs.tf
+++ test/stages/stage4-outputs.tf
@@ -5,13 +5,13 @@ resource null_resource write_outputs {
 
     environment = {
       OUTPUT = jsonencode({
-        name        = module.gitops_module.name
-        branch      = module.gitops_module.branch
-        namespace   = module.gitops_module.namespace
-        server_name = module.gitops_module.server_name
-        layer       = module.gitops_module.layer
-        layer_dir   = module.gitops_module.layer == "infrastructure" ? "1-infrastructure" : (module.gitops_module.layer == "services" ? "2-services" : "3-applications")
-        type        = module.gitops_module.type
+        name        = module.db2u.name
+        branch      = module.db2u.branch
+        namespace   = module.db2u.namespace
+        server_name = module.db2u.server_name
+        layer       = module.db2u.layer
+        layer_dir   = module.db2u.layer == "infrastructure" ? "1-infrastructure" : (module.db2u.layer == "services" ? "2-services" : "3-applications")
+        type        = module.db2u.type
       })
     }
   }
