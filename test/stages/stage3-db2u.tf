1cde423a2b88ae923fa3555ad659412434d6a858
Yogendra Srivastava <ysrivastava@in.ibm.com>
2022-01-28 23:23:01 +0530

    updated namespace
    Signed-off-by: Yogendra Srivastava <ysrivastava@in.ibm.com>

--- test/stages/stage3-db2u.tf
+++ test/stages/stage3-db2u.tf
@@ -9,6 +9,6 @@ module "db2u" {
   gitops_config = module.gitops.gitops_config
   git_credentials = module.gitops.git_credentials
   server_name = module.gitops.server_name
-  namespace = module.gitops_namespace.name
+  operator_namespace = module.gitops_namespace.name
   kubeseal_cert = module.gitops.sealed_secrets_cert
 }
