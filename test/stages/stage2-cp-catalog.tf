9d6c0af25d4638922ad80d8393722b7584f9c521
Andrew Trice <amtrice@us.ibm.com>
2022-02-11 15:25:56 -0500

    pointed at new cp4d cluster
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>

--- test/stages/stage2-cp-catalog.tf
+++ test/stages/stage2-cp-catalog.tf
@@ -1,11 +1,12 @@
 
+## NOT NEEDED FOR CP4D CLUSTER
 
-module "gitops_ibm_catalogs" {
-  source = "github.com/cloud-native-toolkit/terraform-gitops-cp-catalogs.git"
+# module "gitops_ibm_catalogs" {
+#   source = "github.com/cloud-native-toolkit/terraform-gitops-cp-catalogs.git"
 
-  gitops_config = module.gitops.gitops_config
-  git_credentials = module.gitops.git_credentials
-  server_name = module.gitops.server_name
-  kubeseal_cert = module.gitops.sealed_secrets_cert
-  entitlement_key = var.cp_entitlement_key
-}
+#   gitops_config = module.gitops.gitops_config
+#   git_credentials = module.gitops.git_credentials
+#   server_name = module.gitops.server_name
+#   kubeseal_cert = module.gitops.sealed_secrets_cert
+#   entitlement_key = var.cp_entitlement_key
+# }
