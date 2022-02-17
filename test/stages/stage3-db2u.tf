3a184619acee56ee3094d4268614ba847b71ef92
Yogendra Srivastava <ysrivastava@in.ibm.com>
2022-02-17 12:53:31 +0530

    updated ibm-db2u-operator
    Signed-off-by: Yogendra Srivastava <ysrivastava@in.ibm.com>

--- test/stages/stage3-db2u.tf
+++ test/stages/stage3-db2u.tf
@@ -8,8 +8,7 @@ module "db2u" {
 
   gitops_config = module.gitops.gitops_config
   git_credentials = module.gitops.git_credentials
-  server_name = module.gitops.server_name
-  namespace = module.gitops_namespace.name
+  server_name = module.gitops.server_name  
   operator_namespace = "cpd-operators"
   kubeseal_cert = module.gitops.sealed_secrets_cert
 }
